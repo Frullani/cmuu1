303445533a47a6b371b2a7c57b1bc42eae09ccd9
Frullani <35868245+Frullani@users.noreply.github.com>
2023-10-10 00:03:37 +0300

    Initial Commit

---
+++ cmuu1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  cmuu1
+//
+//  Created by Arsenii Klevakin on 10.10.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
